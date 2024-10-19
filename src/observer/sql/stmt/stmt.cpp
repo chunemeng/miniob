22bf09625d0984893a5ce101da533c96265a1b51
chunemeng <chunemeng@outlook.com>
2024-10-19 21:57:21 +0800

    fix task3 lost header

--- src/observer/sql/stmt/stmt.cpp
+++ src/observer/sql/stmt/stmt.cpp
@@ -20,6 +20,7 @@ See the Mulan PSL v2 for more details. */
 #include "sql/stmt/delete_stmt.h"
 #include "sql/stmt/desc_table_stmt.h"
 #include "sql/stmt/exit_stmt.h"
+#include "sql/stmt/drop_table_stmt.h"
 #include "sql/stmt/explain_stmt.h"
 #include "sql/stmt/help_stmt.h"
 #include "sql/stmt/insert_stmt.h"
